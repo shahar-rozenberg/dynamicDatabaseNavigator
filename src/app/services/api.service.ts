09f69de6e370f949b3c8e4da1968c1aa27a0cf69
shahar_rozenberg <shahar25996@gmail.com>
2020-11-08 21:41:35 +0200

    remove comments

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,13 +6,12 @@ import {NavigatorLevel} from '../models/navigator-level';
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor(private httpClient: HttpClient) {
-  }
-
   private _url: string = 'assets/data.json';
   private _dataCache;
   private readonly _firstLevelIndexes: number[] = [1, 2, 3];
+
+  constructor(private httpClient: HttpClient) {
+  }
 
   public async getRowChildren(childrenIds: number[] = this._firstLevelIndexes) {
     const children: Map<number, NavigatorLevel> = new Map();
@@ -26,22 +25,4 @@ export class ApiService {
   private fetchData() {
     return this.httpClient.get(this._url);
   }
-
-  // public async getRowChildren(childrenIds: number[] = this._firstLevelIndexes) {
-  //   const children: Map<number, NavigatorLevel> = new Map();
-  //
-  //   if (!this._dataCache) {
-  //     await this.fetchData().then(response => response.json()).then((firstLevelData) => {
-  //       this._dataCache = firstLevelData;
-  //     });
-  //   }
-  //
-  //   childrenIds.forEach((childId: number) => children.set(childId, this._dataCache[childId]));
-  //
-  //   return children;
-  // }
-  //
-  // private fetchData() {
-  //   return fetch(this._url);
-  // }
 }
